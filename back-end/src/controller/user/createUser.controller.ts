a305de19fe783579f353049091da0a20414a435d
24LP7490 24LP7490 <24LP7490@G301-CC0188.local>
2025-04-09 16:37:29 +0800

    backend prisma setup

---
+++ back-end/src/controller/user/createUser.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import prisma from '../../prismaClient';
+
+export const createUser = async (req: Request, res: Response) => {
+    const {email, password, username} = req.body
+    try {
+        await prisma.user.create({})
+    } catch (error) {
+        
+    }
+}
